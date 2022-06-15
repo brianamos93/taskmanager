0472d9fdddf6e05cb3ff85eb9e28a6699b40a554
Brian Amos <brianizzo93@gmail.com>
2022-06-15 03:46:13 +0000

    added basics

--- index.js
+++ index.js
@@ -9,20 +9,26 @@
  * App Variables
  */
  const app = express();
- const port = process.env.PORT || "3000";
+ const port = process.env.PORT || "8000";
 /**
  *  App Configuration
  */
-
+ app.set("views", path.join(__dirname, "views"));
+ app.set("view engine", "pug");
+ app.use(express.static(path.join(__dirname, "public")));
 /**
  * Routes Definitions
  */
  app.get("/", (req, res) => {
-    res.status(200).send("WHATABYTE: Food For Devs");
-  });
+  res.render("index", { title: "Home" });
+});
+
+app.get("/user", (req, res) => {
+  res.render("user", { title: "Profile", userProfile: { nickname: "Auth0" } });
+});
 /**
  * Server Activation
  */
- app.get("/", (req, res) => {
-    res.status(200).send("WHATABYTE: Food For Devs");
-  });
+ app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
